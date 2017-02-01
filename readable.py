ac08b18cb85dcb6c389bdabc1379bbd71925756c
Evan Misshula <EvanMisshula@gmail.com>
2017-02-01 07:58:54 -0500

    adds readable examples

---
+++ readable.py
@@ -0,0 +1,20 @@
+long_winded_computation = (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 +
+13 + 14 + 15 + 16 + 17 + 18 + 19 + 20)
+print long_winded_computation
+
+# and for making code easier to read:
+list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+easier_to_read_list_of_lists = [ [1, 2, 3],
+                                 [4, 5, 6],
+                                 [7, 8, 9] ]
+# You can also use a backslash to indicate that a
+# statement continues onto the next line,
+# although we’ll rarely do this:
+two_plus_three = 2 + \
+                 3
+print two_plus_three
+
+for i in [1, 2, 3, 4, 5]:
+# notice the blank line
+print i
+#into the ordinary Python shell, you
