89496e4009a9ba375b3e3c51ef201b862fb81971
Evan Misshula <EvanMisshula@gmail.com>
2017-02-01 18:39:50 -0500

    adds sets

---
+++ 9-sets.py
@@ -0,0 +1,16 @@
+## Sets are distinct elements
+
+nother data strus = set()
+s.add(1)
+print s 
+s.add(2)
+print s 
+s.add(2)
+print s
+
+x = len(s)
+print x
+y = 2 in s
+z = 3 in s
+print y
+print x
