0393230e2a8322f9b6ceeebc2b3a4b3e8f6de3cf
Evan Misshula <EvanMisshula@gmail.com>
2017-02-01 18:41:28 -0500

    adds booleans

---
+++ 11-truthiness.py
@@ -0,0 +1,16 @@
+## Booleans
+
+one_is_less_than_two = 1 < 2
+true_equals_false = True == False
+
+# None
+x = None
+print x == None
+print x is None
+ 
+# all and any
+print all([True, 1, { 3 }])
+print all([True, 1, {}])
+print any([True, 1, {}])
+print all([])
+print any([])
